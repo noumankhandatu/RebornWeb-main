91f8c89b202ae1a2c88680bbbce147304aa52475
TMS <moidzaeem@gmail.com>
2024-07-07 02:04:14 +0500

    done

--- src/app/(user-profile)/settings/page.jsx
+++ src/app/(user-profile)/settings/page.jsx
@@ -4,96 +4,106 @@ import { redirect } from "next/navigation";
 import { parseCookies } from 'nookies'
 import React, { useEffect, useState } from 'react'
 import { useUser } from '../../../../lib/UserConext';
+import axios from 'axios';
 
 const Page = () => {
     const userData = useUser();  
-    const [userInfor,setUserInfor] = useState({
-        name:'',
-        email:'',
-      })
-  useEffect(() =>{
-    const cookies = parseCookies();
+    const [userInfor, setUserInfor] = useState({
+        name: '',
+        email: '',
+        picture: null,
+    });
+    const [preview, setPreview] = useState(null);
 
-    if(userData){
-        setUserInfor({
-            name:userData?.data.name,
-            email:userData?.data.email,
-        })
-       }
+    useEffect(() => {
+        const cookies = parseCookies();
+        console.log('this is', userData);
 
-    const accessToken = cookies?.access_token;
-    if(!accessToken){
-      redirect('/login')
-    }
-  },[userData])
+        if (userData) {
+            setUserInfor({
+                name: userData?.data.name,
+                email: userData?.data.email,
+            });
+        }
 
+        const accessToken = cookies?.access_token;
+        if (!accessToken) {
+            redirect('/login');
+        }
+    }, [userData]);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setUserInfor({ ...userInfor, picture: file });
+        setPreview(URL.createObjectURL(file));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Create a FormData object to send the file
+        const formData = new FormData();
+        formData.append('user_id', userData?.data.id);
+        formData.append('name', userInfor?.name);
+
+        if (userInfor.picture) {
+            formData.append('image', userInfor.picture);
+        }
+        const apiUrl = process.env.API_URL;
+
+
+        try {
+            const response = await axios.post(`https://backend.reborngreen.org/upload-image`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            console.log('Upload successful:', response.data);
+            // Handle successful upload
+        } catch (error) {
+            console.error('Upload failed:', error);
+            // Handle upload error
+        }
+    };
 
     return (
         <div className='overflow-hidden h-full'>
-
-
-
-
-        
             <section className='w-full flex items-start justify-start mt-20 min-h-screen '>
-               <LeftSideBar />
+                <LeftSideBar />
                 <main className='max-w-[1800px] flex-1 bg-[#fbfbfb] px-[0px] lg:px-[60px] xll:px-[120px] py-[2rem] mx-auto h-full'>
-
-                    <h3 className=' text-center leading-normal my-7 lg:leading-[50px] xll:leading-[60px] text-[30px] lg:text-[40px] xll:text-[50px] text-black-text font-medium font-worksans'> Settings</h3>
-
+                    <h3 className=' text-center leading-normal my-7 lg:leading-[50px] xll:leading-[60px] text-[30px] lg:text-[40px] xll:text-[50px] text-black-text font-medium font-worksans'>Settings</h3>
                     <div className='w-full flex flex-col gap-5 items-center justify-center'>
-                        <form className=' w-[240px] ssm:w-[300px] xsm:max-w-[600px] px-3 sm:px-10 py-20 rounded-lg sm:w-[600px] bg-white shadow-lg'>
+                        <form className='w-[240px] ssm:w-[300px] xsm:max-w-[600px] px-3 sm:px-10 py-20 rounded-lg sm:w-[600px] bg-white shadow-lg' onSubmit={handleSubmit}>
                             <h2 className='text-left font-worksans text-[20px] xsm:text-[25px] lg:text-[30px] xll:text-[35px] uppercase font-semibold text-[#3d3d3d]'>Personal Details</h2>
                             <div>
                                 <div className='flex my-4 flex-col'>
-                                    <label className='mb-1'>Company Name</label>
-                                    <input type="text" placeholder='full name' className='a company-none border-2 rounded-md shadow-lg border-[#443d3d82] px-4 py-2' />
-                                </div>
-                                <div className='flex my-4 flex-col'>
                                     <label className='mb-1'>Full Name</label>
-                                    <input onChange={(e) => setUserInfor({name:e.target.value})} value={userInfor?.name} type="text" placeholder='full name' className='outline-none border-2 rounded-md shadow-lg border-[#443d3d82] px-4 py-2' />
+                                    <input onChange={(e) => setUserInfor({ ...userInfor, name: e.target.value })} value={userInfor?.name} type="text" placeholder='full name' className='outline-none border-2 rounded-md shadow-lg border-[#443d3d82] px-4 py-2' />
                                 </div>
                                 <div className='flex my-4 flex-col'>
                                     <label className='mb-1'>Email</label>
                                     <input value={userInfor?.email} readOnly type="email" placeholder='Email' className='outline-none border-2 rounded-md shadow-lg border-[#443d3d82] px-4 py-2' />
                                 </div>
+                                <div className='flex my-4 flex-col'>
+                                    <label className='mb-1'>Upload Picture</label>
+                                    <input type="file" onChange={handleFileChange} className='outline-none border-2 rounded-md shadow-lg border-[#443d3d82] px-4 py-2' />
+                                </div>
+                                {preview && (
+                                    <div className='flex my-4 flex-col'>
+                                        <img src={preview} alt="Selected Preview" className='rounded-md shadow-lg max-w-full h-auto' />
+                                    </div>
+                                )}
                             </div>
-
                             <div className='flex flex-col gap-4 items-center justify-start w-full'>
                                 <button className='w-full px-7 py-3 bg-blue-900 hover:bg-blue-700 duration-300 text-white rounded-lg font-poppins text-[16px] xll:text-[18px] font-medium tracking-wide'>Save</button>
                             </div>
                         </form>
-
-
-
-
                     </div>
-
-
-                   
-                    <div className='my-12 flex items-center justify-start flex-col gap-6'>
-                    <h3 className=' text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[30px] lg:text-[40px] xll:text-[50px] text-black-text font-medium font-worksans'>Your Packages</h3>
-                    <div className=' w-full sm:w-[70%] flex items-center justify-center flex-col gap-4 px-4 text-center py-5 bg-white border shadow-lg min-h-[200px]'>
-                        <p className=' text-[18px] xsm:text-[20px]'>200 trees for £200.00 / month </p>
-                        <button className='text-white rounded-md bg-blue-700 hover:bg-blue-500 duration-200 px-8 py-3 text-lg font-semibold'>Inactive</button>
-                    </div>
-                    </div>
-
-
-
-                    {/* <div className='my-12 flex items-center justify-start flex-col gap-6'>
-                    <h3 className=' text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[30px] lg:text-[40px] xll:text-[50px] text-black-text font-medium font-worksans'>Account Management</h3>
-                    <div className=' w-full sm:w-[70%] flex items-center justify-center flex-col gap-1 px-4 text-center py-5 bg-white border shadow-lg min-h-[200px]'>
-                        <p className=' text-[18px] xsm:text-[20px]'>Would you like to close your account with Treeapp?</p>
-                        <button className='text-red-600 hover:text-red-400 duration-200 px-6 py-3 rounded-sm text-lg font-semibold'>Delete Your Account</button>
-                    </div>
-                    </div> */}
-
-
                 </main>
             </section>
         </div>
-    )
-}
+    );
+};
 
-export default Page
+export default Page;
--- src/app/(user-profile)/user-profile/page.jsx
+++ src/app/(user-profile)/user-profile/page.jsx
@@ -1,169 +1,173 @@
-'use client'
-import LeftSideBar from '@/components/LeftSideBar'
+"use client";
+import LeftSideBar from "@/components/LeftSideBar";
 import { redirect } from "next/navigation";
-import Image from 'next/image'
-import Link from 'next/link'
-import { parseCookies } from 'nookies'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import toast from 'react-hot-toast';
-import { useUser } from '../../../../lib/UserConext';
-import ScriptGenrate from '@/components/ScriptGenrate';
+import Image from "next/image";
+import Link from "next/link";
+import { parseCookies } from "nookies";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useUser } from "../../../../lib/UserConext";
+import ScriptGenrate from "@/components/ScriptGenrate";
 const Page = () => {
-
-  const userData = useUser();  
+  const userData = useUser();
 
   const [disable, setDiable] = useState(false);
-  const [name,setName] = useState('')
-  const [userApi,setUserApi] = useState(null)
-  const [accessToken,setAccessToken] = useState(null)
-  const [isGenrateApi,setIsGenrateApi] = useState(false)
-  const [treePlanted,setTreePlanted] = useState(0)
-  const [climatePoints,setClimatePoints] = useState(0)
+  const [name, setName] = useState("");
+  const [userApi, setUserApi] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [isGenrateApi, setIsGenrateApi] = useState(false);
+  const [treePlanted, setTreePlanted] = useState(0);
+  const [climatePoints, setClimatePoints] = useState(0);
+  const [profilePicture, setProfilePicture] = useState('')
 
-  useEffect(() =>{
+  useEffect(() => {
     const cookies = parseCookies();
     const name = cookies?.user_name;
 
-    setName(name)
-    if(userData !== undefined){
-      setUserApi(userData?.data.api_key)
+    setName(name);
+    if (userData !== undefined) {
+      setUserApi(userData?.data.api_key);
+      if(userData?.data.logo)
+      setProfilePicture(`https://backend.reborngreen.org/${userData?.data.logo}`); // Assuming profile picture URL is in userData
+
     }
-    if(userApi !== undefined && userApi !== null){
-      setIsGenrateApi(true)
-      console.log(isGenrateApi,userApi)
+    if (userApi !== undefined && userApi !== null) {
+      setIsGenrateApi(true);
+      console.log(isGenrateApi, userApi);
     }
     const accessToken = cookies?.access_token;
-    if(!accessToken){
-      redirect('/login')
-    }else{
-      setAccessToken(accessToken)
+    if (!accessToken) {
+      redirect("/login");
+    } else {
+      setAccessToken(accessToken);
     }
 
- if(userApi){
-  const getTreeData = async () => {
-    try {
-        const apiUrl = `${process.env.API_URL}/user/tree-record?api_key=${userApi}`;
-        const response = await fetch(apiUrl, {
-            method: 'GET',
+    if (userApi) {
+      const getTreeData = async () => {
+        try {
+          const apiUrl = `${process.env.API_URL}/user/tree-record?api_key=${userApi}`;
+          const response = await fetch(apiUrl, {
+            method: "GET",
             headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        });
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
 
-        if (response.ok) {
+          if (response.ok) {
             const data = await response.json();
-            if(data.status !== 200){
-              // toast.error(data.message) 
+            if (data.status !== 200) {
+              // toast.error(data.message)
               setDiable(false);
-            }else{
-              setTreePlanted(data.data.tree_planted)   
-              setClimatePoints(data.data.climate_points)   
+            } else {
+              setTreePlanted(data.data.tree_planted);
+              setClimatePoints(data.data.climate_points);
               setDiable(false);
             }
-          
           } else {
-            
             setDiable(false);
-            toast.error('Request failed');
-        }
-    } catch (error) {
-        console.error('Request failed:', error.message);
-        setDiable(false);
-    }
-};
-getTreeData()
-
-
-
- }
-
-
-
-  
-  },[userData,userApi,isGenrateApi])
-
-  const generateApi = async () => { 
-    setDiable(true)
-      try {
-        
-        const apiUrl = process.env.API_URL;
-        const response = await fetch(`${apiUrl}/user/api-key`, {
-            method: 'PATCH',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        });
-
-        if (response.ok) {
-            const data = await response.json();        
-           setIsGenrateApi(true)
-           toast.success("Successfuly Genrate User Api")
-           setDiable(false);
-          //  redirect('/user-profile')
-          } else {
-            // Handle error response
-            console.log(response)
+            toast.error("Request failed");
           }
         } catch (error) {
-          console.error('Request failed:', error.message);
+          console.error("Request failed:", error.message);
           setDiable(false);
         }
-      
-};
+      };
+      getTreeData();
+    }
+  }, [userData, userApi, isGenrateApi]);
 
+  const generateApi = async () => {
+    setDiable(true);
+    try {
+      const apiUrl = process.env.API_URL;
+      const response = await fetch(`${apiUrl}/user/api-key`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-// pages/utils/generateScript.js
+      if (response.ok) {
+        const data = await response.json();
+        setIsGenrateApi(true);
+        toast.success("Successfuly Genrate User Api");
+        setDiable(false);
+        //  redirect('/user-profile')
+      } else {
+        // Handle error response
+        console.log(response);
+      }
+    } catch (error) {
+      console.error("Request failed:", error.message);
+      setDiable(false);
+    }
+  };
 
-
-
-
-
-
-
+  // pages/utils/generateScript.js
 
   return (
-    <div className='overflow-hidden h-full'>
-
-  
-
-
-    {/* about sec 1  */}
-    <section className='w-full flex items-start justify-start mt-20 min-h-screen '>
-               <LeftSideBar />
-               <main className='max-w-[1800px] flex-1 bg-[#fbfbfb] px-[0px] lg:px-[60px] xll:px-[120px] py-[2rem] mx-auto h-full'>
-          <h3 className=' text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[25px] lg:text-[35px] xll:text-[45px] text-black-text font-medium font-worksans'>Welcome <span className='text-green font-semibold'>{name && name}</span></h3>
-{/* {
+    <div className="overflow-hidden h-full">
+      {/* about sec 1  */}
+      <section className="w-full flex items-start justify-start mt-20 min-h-screen ">
+        <LeftSideBar />
+        <main className="max-w-[1800px] flex-1 bg-[#fbfbfb] px-[0px] lg:px-[60px] xll:px-[120px] py-[2rem] mx-auto h-full">
+          <h3 className=" text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[25px] lg:text-[35px] xll:text-[45px] text-black-text font-medium font-worksans">
+            Welcome{" "}
+            <span className="text-green font-semibold">{name && name}</span>
+          </h3>
+          {profilePicture && (
+            <div className='flex justify-center mt-4'>
+              <Image src={profilePicture} alt='Profile Picture' width={100} height={100} className='rounded-full' />
+            </div>
+          )}
+          {/* {
   isGenrateApi && <ScriptGenrate userApi={userApi}  />
 } */}
-          <div className='w-full flex flex-col gap-5 items-center justify-center'>
-          <div style={{ backgroundImage: "url('/assets/images/treeplantmain.jpg')" }} className='bg-white w-[90%] flex relative items-center flex-col justify-center px-3 h-[300px] mt-4 shadow-2xl py-10 bg-cover bg-no-repeat rounded-xl '>
-                <h1 className='z-10 text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[25px] lg:text-[35px] xll:text-[45px] text-white font-medium font-worksans'>{treePlanted} trees planted</h1>
+          <div className="w-full flex flex-col gap-5 items-center justify-center">
+            <div
+              style={{
+                backgroundImage: "url('/assets/images/treeplantmain.jpg')",
+              }}
+              className="bg-white w-[90%] flex relative items-center flex-col justify-center px-3 h-[300px] mt-4 shadow-2xl py-10 bg-cover bg-no-repeat rounded-xl "
+            >
+              <h1 className="z-10 text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[25px] lg:text-[35px] xll:text-[45px] text-white font-medium font-worksans">
+                {treePlanted} trees planted
+              </h1>
 
-                <div className='absolute overflow-hidden rounded-xl z-0 top-0 left-0 w-full h-full bg-[#000] opacity-[0.4]'></div>
+              <div className="absolute overflow-hidden rounded-xl z-0 top-0 left-0 w-full h-full bg-[#000] opacity-[0.4]"></div>
+            </div>
+            {!isGenrateApi && (
+              <div className="flex items-center justify-center w-full my-6">
+                <button
+                  disabled={disable}
+                  onClick={() => generateApi()}
+                  className={` ${
+                    disable && "opacity-50"
+                  } text-lg bg-secondary font-semibold hover:bg-green cursor-pointer px-4 py-3 rounded-lg text-white`}
+                >
+                  Genrate a Key
+                </button>
+              </div>
+            )}
+
+            <div
+              style={{ backgroundImage: "url('/assets/images/reforest.jpg')" }}
+              className="bg-white w-[90%] flex relative items-center flex-col justify-center px-3 h-[300px] mt-4 shadow-2xl py-10 bg-cover bg-no-repeat rounded-xl "
+            >
+              <h1 className="z-10 text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[25px] lg:text-[35px] xll:text-[45px] text-white font-medium font-worksans">
+                You have {climatePoints} climate points
+              </h1>
+
+              {/* <Link href='' className='text-white text-2xl underline my-3 z-20 font-semibold' >See All Assets</Link> */}
+
+              <div className="absolute overflow-hidden rounded-xl z-0 top-0 left-0 w-full h-full bg-[#000] opacity-[0.4]"></div>
+            </div>
           </div>
-          {
-            !isGenrateApi && 
-            <div className='flex items-center justify-center w-full my-6'>
-            <button disabled={disable} onClick={() => generateApi()}  className={` ${disable && 'opacity-50'} text-lg bg-secondary font-semibold hover:bg-green cursor-pointer px-4 py-3 rounded-lg text-white`}>Genrate a Key</button>
-          </div>
-          }
-         
-          <div style={{ backgroundImage: "url('/assets/images/reforest.jpg')" }} className='bg-white w-[90%] flex relative items-center flex-col justify-center px-3 h-[300px] mt-4 shadow-2xl py-10 bg-cover bg-no-repeat rounded-xl '>
-                <h1 className='z-10 text-center leading-normal lg:leading-[50px] xll:leading-[60px] text-[25px] lg:text-[35px] xll:text-[45px] text-white font-medium font-worksans'>You have {climatePoints} climate points</h1>
-
-                {/* <Link href='' className='text-white text-2xl underline my-3 z-20 font-semibold' >See All Assets</Link> */}
-
-                <div className='absolute overflow-hidden rounded-xl z-0 top-0 left-0 w-full h-full bg-[#000] opacity-[0.4]'></div>
-          </div>
-
-          </div>
-     
-
         </main>
-    </section>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
